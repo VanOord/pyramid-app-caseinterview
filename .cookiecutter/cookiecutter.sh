d2313458fc82a41b09f78d70e2cf3af7592153e1
hoonhout <bas@hoonhout.com>
2023-03-21 15:45:57 +0100

    Move cookiecutter files to separate dir

---
+++ .cookiecutter/cookiecutter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+pip install cookiecutter --upgrade
+REPLAYFILE=cookiecutter.replay.json
+REPLAYFILESRC=~/.cookiecutter_replay/template.json
+TMPDIR=$(mktemp -u)
+echo "Using working directory: $TMPDIR..."
+mkdir -p $TMPDIR/template/{{cookiecutter.project_repo}}
+cp -r . $TMPDIR/template/{{cookiecutter.project_repo}}
+rm -rf $TMPDIR/template/{{cookiecutter.project_repo}}/.git
+rm -rf $TMPDIR/template/{{cookiecutter.project_repo}}/.cookiecutter
+cp -r .cookiecutter/* $TMPDIR/template
+mkdir -p $TMPDIR/rendered
+if [ -f "$REPLAYFILE" ]; then
+    echo "Using replay file: $REPLAYFILE"
+    cookiecutter -f --replay-file ./$REPLAYFILE -o $TMPDIR/rendered $TMPDIR/template
+else 
+    cookiecutter -f -o $TMPDIR/rendered $TMPDIR/template
+fi
+cp -r $TMPDIR/rendered/*/* .
+if [ -f "$REPLAYFILESRC" ]; then
+    cp -r $REPLAYFILESRC $REPLAYFILE
+fi
+rm -rf $TMPDIR
