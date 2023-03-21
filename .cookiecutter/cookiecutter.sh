628994849ae68e1ff09fceef0313204b6362438f
hoonhout <bas@hoonhout.com>
2023-03-21 16:38:56 +0100

    Add git statments

--- .cookiecutter/cookiecutter.sh
+++ .cookiecutter/cookiecutter.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+# link template repo and merge latest changes
+git remote -v | grep -w template || git remote add template https://github.com/VanOord/template-pyramid-app.git
+git merge template/master --allow-unrelated-histories --strategy-option theirs --no-commit --no-ff
+
+# create cookiecutter environment
 pip install cookiecutter --upgrade
 REPLAYFILE=.cookiecutter/cookiecutter.replay.json
 REPLAYFILESRC=~/.cookiecutter_replay/template.json
+
+# create template environment
 TMPDIR=$(mktemp -u)
 echo "Using working directory: $TMPDIR..."
 mkdir -p $TMPDIR/template/{{cookiecutter.project_repo}}
@@ -10,15 +18,21 @@ rm -rf $TMPDIR/template/{{cookiecutter.project_repo}}/.git
 rm -rf $TMPDIR/template/{{cookiecutter.project_repo}}/.cookiecutter
 cp -r .cookiecutter/* $TMPDIR/template
 mkdir -p $TMPDIR/rendered
+
+# apply cookiecutter
 if [ -f "$REPLAYFILE" ]; then
     echo "Using replay file: $REPLAYFILE"
     cookiecutter -f -o $TMPDIR/rendered $TMPDIR/template --replay-file ./$REPLAYFILE
 else 
     cookiecutter -f -o $TMPDIR/rendered $TMPDIR/template
 fi
+
+# copy result
 cp -r $TMPDIR/rendered/*/* .
 if [ -f "$REPLAYFILESRC" ]; then
     cp -r $REPLAYFILESRC $REPLAYFILE
 fi
+
+# clean up
 rm -rf {{*}}
 rm -rf $TMPDIR
