ac5206f89ee9c7866100a1be2cd24adea4898a1b
Ryan Alvita <ryan.alvita@vanoord.com>
2024-12-12 06:42:18 +0100

    Remove .template and running template render

--- .template/render.bat
+++
@@ -1,3 +0,0 @@
-echo off
-pip install -r .template/requirements.txt --upgrade --user
-python .template/render.py
--- .template/render.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pip install -r .template/requirements.txt --upgrade --user
-python .template/render.py
